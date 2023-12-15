eab3fcec1c1670a61dacee087bca4f11608ebcac
Fumihiko Hata <hatappo@users.noreply.github.com>
2023-12-16 00:29:47 +0900

    feat: add test and task runner

---
+++ 9cc/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+assert() {
+  expected="$1"
+  input="$2"
+
+  ./9cc "$input" > tmp.s  # compile to assembly
+  cc tmp.s -o tmp         # compile to executable binary
+  ./tmp                   # execute
+  actual="$?"
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+assert 0 0
+assert 42 42
+
+echo OK
