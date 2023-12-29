31f739dfe2d798c2fbacd1b0ac14ab5fad437c79
Fumihiko Hata <hatappo@users.noreply.github.com>
2023-12-29 21:54:09 +0900

    complete step 5

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -104,7 +104,9 @@ Token* tokenize() {
       continue;
     }
 
-    if (*p == '+' || *p == '-') {
+    // Punctuator
+    if (strchr("+-*/()", *p)) {
+      // if (*p == '+' || *p == '-') {
       cur = new_token(TK_RESERVED, cur, p++);
       continue;
     }
@@ -122,37 +124,141 @@ Token* tokenize() {
   return head.next;
 }
 
+// 抽象構文木のノードの種類
+typedef enum {
+  ND_ADD,  // +
+  ND_SUB,  // -
+  ND_MUL,  // *
+  ND_DIV,  // /
+  ND_NUM,  // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+// 抽象構文木のノードの型
+struct Node {
+  NodeKind kind;  // ノードの型
+  Node* lhs;      // 左辺 left-hand side
+  Node* rhs;      // 右辺 right-hand side
+  int val;        // kndがND_NUMの場合のみ使う
+};
+
+Node* new_node(NodeKind kind) {
+  Node* node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  return node;
+}
+
+Node* new_binary(NodeKind kind, Node* lhs, Node* rhs) {
+  Node* node = new_node(kind);
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node* new_num(int val) {
+  Node* node = new_node(ND_NUM);
+  node->val = val;
+  return node;
+}
+
+Node* expr();
+Node* mul();
+Node* primary();
+
+// expr    = mul ("+" mul | "-" mul)*
+Node* expr() {
+  Node* node = mul();
+  for (;;) {
+    if (consume('+'))
+      node = new_binary(ND_ADD, node, mul());
+    else if (consume('-'))
+      node = new_binary(ND_SUB, node, mul());
+    else
+      return node;
+  }
+}
+
+// mul     = primary ("*" primary | "/" primary)*
+Node* mul() {
+  Node* node = primary();
+
+  for (;;) {
+    if (consume('*'))
+      node = new_binary(ND_MUL, node, primary());
+    else if (consume('/'))
+      node = new_binary(ND_DIV, node, primary());
+    else
+      return node;
+  }
+}
+
+// primary = num | "(" expr ")"
+Node* primary() {
+  // 次のトークンが"("なら、"(" expr ")"のはず
+  if (consume('(')) {
+    Node* node = expr();
+    expect(')');
+    return node;
+  }
+
+  // そうでなければ数値のはず
+  return new_num(expect_number());
+}
+
+void gen(Node* node) {
+  if (node->kind == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->kind) {
+    case ND_ADD:
+      printf("  add rax, rdi\n");
+      break;
+    case ND_SUB:
+      printf("  sub rax, rdi\n");
+      break;
+    case ND_MUL:
+      printf("  imul rax, rdi\n");
+      break;
+    case ND_DIV:
+      printf("  cqo\n");
+      printf("  idiv rdi\n");
+      break;
+  }
+
+  printf("  push rax\n");
+}
+
 int main(int argc, char** argv) {
   if (argc != 2) {
     error_at(token->str, "引数の個数が正しくありません");
     return 1;
   }
 
-  // トークナイズする
+  // トークナイズしてパースする
   user_input = argv[1];
   token = tokenize();
+  Node* node = expr();
 
   // アセンブリの前半部分を出力
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
 
-  // 式の最初は数でなければならないので、それをチェックして
-  // 最初のmov命令を出力
-  printf("  mov rax, %d\n", expect_number());
+  // 抽象構文木を下りながらコードを生成
+  gen(node);
 
-  // `+ <数>`あるいは`- <数>`というトークンの並びを消費しつつ
-  // アセンブリを出力
-  while (!at_eof()) {
-    if (consume('+')) {
-      printf("  add rax, %d\n", expect_number());
-      continue;
-    }
-
-    expect('-');
-    printf("  sub rax, %d\n", expect_number());
-  }
-
+  // スタックトップに式全体の値が残っているはずなので
+  // それをRAXにロードして関数からの返り値とする
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
--- 9cc/test.sh
+++ 9cc/test.sh
@@ -20,9 +20,16 @@ assert 0 0
 assert 42 42
 assert 21 "5+20-4"
 assert 41 " 12 + 34 - 5 "
+
 # assert 0 "256"
 # assert 0 "1+3++" # トークナイズエラー
 # assert 0 "1 + foo + 5" # トークナイズエラー
 
+assert 105 '21 * 5'
+
+assert 47 '5+6*7'
+assert 15 '5*(9-6)'
+assert 4 '(3+5)/2'
+
+
 echo OK
-g
